f3386403f71bc0f4084a5ee50d58ac6a08580357
Admin <admin@unknown026b0d4e160a.home>
2025-10-21 11:47:18 +0300

    Add all emulator cores to Swift Package

--- Package.swift
+++ Package.swift
@@ -7,15 +7,25 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        .library(
-            name: "Delta",
-            targets: ["DeltaCore"]
-        )
+        .library(name: "Delta", targets: [
+            "DeltaCore",
+            "GBADeltaCore",
+            "GBCDeltaCore",
+            "NESDeltaCore",
+            "SNESDeltaCore",
+            "N64DeltaCore",
+            "MelonDSDeltaCore",
+            "GPGXDeltaCore"
+        ])
     ],
     targets: [
-        .target(
-            name: "DeltaCore",
-            path: "Cores/DeltaCore"
-        )
+        .target(name: "DeltaCore", path: "Cores/DeltaCore"),
+        .target(name: "GBADeltaCore", path: "Cores/GBADeltaCore"),
+        .target(name: "GBCDeltaCore", path: "Cores/GBCDeltaCore"),
+        .target(name: "NESDeltaCore", path: "Cores/NESDeltaCore"),
+        .target(name: "SNESDeltaCore", path: "Cores/SNESDeltaCore"),
+        .target(name: "N64DeltaCore", path: "Cores/N64DeltaCore"),
+        .target(name: "MelonDSDeltaCore", path: "Cores/MelonDSDeltaCore"),
+        .target(name: "GPGXDeltaCore", path: "Cores/GPGXDeltaCore")
     ]
 )
